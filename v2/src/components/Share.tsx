8c2231ac8335dbc9a192851df90b017688162b6f
Edwin Dharmawan <edwin.dharmawan@dexagroup.com>
2024-06-21 16:57:24 +0700

    fix: trial deploy firebase

--- v2/src/components/Share.tsx
+++ v2/src/components/Share.tsx
@@ -1,8 +1,4 @@
-import { styled } from '@stitches/react';
-import { useEffect, useState } from 'react';
-import { Button, message } from 'antd';
-import { MessageFilled, LinkOutlined } from '@ant-design/icons';
-import CopyToClipboard from 'react-copy-to-clipboard';
+import { useEffect } from 'react';
 import { ConfigsType } from '../configs';
 
 declare global {
@@ -11,136 +7,18 @@ declare global {
   }
 }
 
-const isPortrait = window.matchMedia('(orientation: portrait)').matches;
-
-const Section = styled('section', {
-  background: '#EFEBE9',
-  overflow: 'hidden',
-  position: 'relative',
-});
-
-const Layout = styled('div', {
-  width: '100%',
-  padding: isPortrait ? '20% 0% 10% 5%' : '5% 0% 5% 10%',
-});
-
-const Title = styled('p', {
-  color: '#795548',
-  width: '100%',
-  fontSize: isPortrait ? '2.5em' : '3.5em',
-  margin: 0,
-  fontWeight: '500',
-});
-
-const ButtonGroup = styled('div', {
-  width: '100%',
-  textAlign: 'center',
-  paddingBottom: isPortrait ? '10%' : '5%',
-});
-
-const KakaoTalkShareButton = styled(Button, {
-  background: '#fee500',
-  borderColor: '#fee500',
-  color: '#181600',
-  '&:hover': {
-    backgroundColor: '#fcf07e !important',
-    borderColor: '#fcf07e !important',
-    color: '#17160b !important',
-  },
-  '&:focus': {
-    backgroundColor: '#fcf07e !important',
-    borderColor: '#fcf07e !important',
-    color: '#17160b !important',
-  },
-});
-
-const LinkShareButton = styled(Button, {
-  background: '#53acee',
-  borderColor: '#53acee',
-  color: '#ffffff',
-  '&:hover': {
-    backgroundColor: '#9fcbed !important',
-    borderColor: '#9fcbed !important',
-    color: '#ffffff !important',
-  },
-  '&:focus': {
-    backgroundColor: '#9fcbed !important',
-    borderColor: '#9fcbed !important',
-    color: '#ffffff !important',
-  },
-});
 
 type ShareProps = {
   config: ConfigsType;
 };
 
 const Share = ({ config }: ShareProps) => {
-  const [shareCount, setShareCount] = useState<number>(0);
-
-  if (!window.Kakao.isInitialized()) {
-    window.Kakao.init(config.kakaoToken);
-  }
 
   useEffect(() => {
-    if (shareCount !== 0) {
-      window.Kakao.Share.createDefaultButton({
-        objectType: 'feed',
-        container: '#sendKakao',
-        content: {
-          title: `Anda diundang ke acara pernikahan ${config.groom.name}❤${config.bride.name}`,
-          description: "Klik tombol 'buka undangan pernikahan' di bawah ini untuk membacanya🤵👰",
-          imageUrl: config.kakaoImage,
-          link: {
-            mobileWebUrl: config.url,
-            webUrl: config.url,
-          },
-        },
-        buttons: [
-          {
-            title: 'Buka undangan pernikahan',
-            link: {
-              mobileWebUrl: config.url,
-              webUrl: config.url,
-            },
-          },
-        ],
-        installTalk: true,
-      });
-      setTimeout(() => {
-        document.getElementById('sendKakao')?.click();
-        message.success('Bagikan undangan pernikahan di KakaoTalk!');
-      }, 100);
-    }
-  }, [config, shareCount]);
 
-  return (
-    <Section>
-      <Layout>
-        <Title>Sebarkan Undangan Pernikahan</Title>
-      </Layout>
-      <ButtonGroup>
-        <KakaoTalkShareButton
-          style={{ margin: 8 }}
-          icon={<MessageFilled />}
-          id="sendKakao"
-          size="large"
-          onClick={() => setShareCount(shareCount + 1)}
-        >
-          Bagikan di kakao talk
-        </KakaoTalkShareButton>
-        <CopyToClipboard text={config.url}>
-          <LinkShareButton
-            style={{ margin: 8 }}
-            icon={<LinkOutlined />}
-            size="large"
-            onClick={() => message.success('Tautan undangan pernikahan Anda telah disalin.')}
-          >
-            Bagikan dengan tautan
-          </LinkShareButton>
-        </CopyToClipboard>
-      </ButtonGroup>
-    </Section>
-  );
+  }, []);
+
+  return <></>
 };
 
 export default Share;
--- v2/src/index.tsx
+++ v2/src/index.tsx
@@ -5,6 +5,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBgCWja8gFogb7TlWpvvNMegH8Ow2SIQvc",
+//   authDomain: "intan-edwin.firebaseapp.com",
+//   projectId: "intan-edwin",
+//   storageBucket: "intan-edwin.appspot.com",
+//   messagingSenderId: "923077826017",
+//   appId: "1:923077826017:web:f8e93f8fe97f83c71eaea0",
+//   measurementId: "G-PDF83JY379"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
